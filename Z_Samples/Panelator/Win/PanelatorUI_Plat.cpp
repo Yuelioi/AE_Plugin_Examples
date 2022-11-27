03ccf539561a3552bc4e0c5a05445f6bd98c1972
yueloi <yuelioi1210@gmail.com>
2022-11-28 02:35:07 +0800

    update

--- Z_Samples/Panelator/Win/PanelatorUI_Plat.cpp
+++ Z_Samples/Panelator/Win/PanelatorUI_Plat.cpp
@@ -29,12 +29,12 @@ PanelatorUI_Plat::PanelatorUI_Plat(SPBasicSuite* spbP, AEGP_PanelH panelH,
 	i_prevWindowProc(NULL)
 {
 
-	// hook the main window
-	i_prevWindowProc = (WindowProc)GetWindowLongPtr(platformWindowRef, GWLP_WNDPROC);
-	// blasting the client's wndproc, it's required that they provide an adapter.
+	//// hook the main window
+	//i_prevWindowProc = (WindowProc)GetWindowLongPtr(platformWindowRef, GWLP_WNDPROC);
+	//// blasting the client's wndproc, it's required that they provide an adapter.
 
-	SetWindowLongPtrA(platformWindowRef, GWLP_WNDPROC, (LONG_PTR)PanelatorUI_Plat::StaticOSWindowWndProc);
-	::SetProp(platformWindowRef, OSWndObjectProperty, (HANDLE)this);
+	//SetWindowLongPtrA(platformWindowRef, GWLP_WNDPROC, (LONG_PTR)PanelatorUI_Plat::StaticOSWindowWndProc);
+	//::SetProp(platformWindowRef, OSWndObjectProperty, (HANDLE)this);
 
 
 	POINT pt = { 0,0 };
@@ -161,83 +161,12 @@ LRESULT PanelatorUI_Plat::OSWindowWndProc(HWND	hWnd,
 	// do I want to do something
 	bool handledB = false;
 	switch (message) {
-	case WM_PAINT: {
-		RECT clientArea;
-		GetClientRect(hWnd, &clientArea);
-		HBRUSH brush = NULL;
-		if (i_use_bg) {
-			brush = CreateBrushFromSelector(i_appSuite.get(), PF_App_Color_PANEL_BACKGROUND);
-		}
-		else {
-			brush = CreateSolidBrush(RGB(red, green, blue));
-		}
-		HDC hdc = GetDC(hWnd);
-		FillRect(hdc, &clientArea, brush);
-		DeleteObject(brush);
 
-
-		clientArea.right = clientArea.left + 200;
-		clientArea.bottom = clientArea.top + 30;
-		brush = CreateBrushFromSelector(i_appSuite.get(), PF_App_Color_LIST_BOX_FILL);
-		FillRect(hdc, &clientArea, brush);
-		DeleteObject(brush);
-
-		clientArea.top = clientArea.bottom;
-		clientArea.bottom = clientArea.top + 30;
-		brush = CreateBrushFromSelector(i_appSuite.get(), PF_App_Color_TEXT_ON_LIGHTER_BG);
-		FillRect(hdc, &clientArea, brush);
-		DeleteObject(brush);
-
-		clientArea.top = clientArea.bottom;
-		clientArea.bottom = clientArea.top + 30;
-		brush = CreateBrushFromSelector(i_appSuite.get(), PF_App_Color_DARK_CAPTION_FILL);
-		FillRect(hdc, &clientArea, brush);
-		DeleteObject(brush);
-
-		clientArea.top = clientArea.bottom;
-		clientArea.bottom = clientArea.top + 30;
-		brush = CreateBrushFromSelector(i_appSuite.get(), PF_App_Color_DARK_CAPTION_TEXT);
-		FillRect(hdc, &clientArea, brush);
-		DeleteObject(brush);
-
-		char textToDraw[200];
-		RECT origClientArea;
-		GetClientRect(hWnd, &origClientArea);
-
-#ifdef AE_OS_WIN
-		sprintf_s(textToDraw, "窗口大小: %d, %d", origClientArea.right - origClientArea.left, origClientArea.bottom - origClientArea.top);
-#else
-		sprintf(textToDraw, "窗口大小: %d, %d", origClientArea.right - origClientArea.left, origClientArea.bottom - origClientArea.top);
-#endif
-		clientArea.top = clientArea.bottom;
-		clientArea.bottom = clientArea.top + 30;
-
-		DrawText(hdc, textToDraw, (uint32_t)strlen(textToDraw), &clientArea, DT_SINGLELINE | DT_LEFT);
-
-#ifdef AE_OS_WIN
-		sprintf_s(textToDraw, "点击次数: %d", i_numClicks);
-#else
-		sprintf(textToDraw, "点击次数: %d", i_numClicks);
-#endif
-		clientArea.top = clientArea.bottom;
-		clientArea.bottom = clientArea.top + 30;
-
-		DrawText(hdc, textToDraw, (uint32_t)strlen(textToDraw), &clientArea, DT_SINGLELINE | DT_LEFT);
-
-
-		handledB = true;
-	}
 	case WM_SIZING:
 		InvalidateAll();
 		break;
 
-	case WM_COMMAND:
-		if (HIWORD(wParam) == BN_CLICKED && LOWORD(wParam) == BtnID) {
-			i_numClicks++;
-			InvalidateAll();
-			handledB = true;
-		}
-		break;
+
 	}
 
 
